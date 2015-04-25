502b79068276322e23155bdd6ef09a2dbdcaa57f
reneeb <info@perl-services.de>
2015-04-25 16:39:45 +0200

    show news in a widget

--- Kernel/Output/HTML/OutputFilterProductNews.pm
+++ Kernel/Output/HTML/OutputFilterProductNews.pm
@@ -15,7 +15,9 @@ use warnings;
 use List::Util qw(first);
 
 our @ObjectDependencies = qw(
+    Kernel::Config
     Kernel::System::ProductNews
+    Kernel::System::User
     Kernel::Output::HTML::Layout
 );
 
@@ -47,8 +49,28 @@ sub Run {
     return 1 if !%ProductNews;
 
     my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $UserObject   = $Kernel::OM->Get('Kernel::System::User');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
+    my $UserID      = $LayoutObject->{UserID};
+    my %Preferences = $UserObject->GetPreferences(
+        UserID => $UserID,
+    );
+
+    my %UserReadNews = map{ $_ => 1 } split /;/, $Preferences{ProductNewsRead} || '';
+
+    my $NewsShown = 0;
+
+    my $ShowTeaserWidget    = $ConfigObject->Get('ProductNews::ShowTeaserWidget') || 0;
+    my $ShowCreatedByWidget = $ConfigObject->Get('ProductNews::ShowCreatedByWidget') || 0;
+
+    NEWSID:
     for my $NewsID ( sort keys %ProductNews ) {
+
+        next NEWSID if $UserReadNews{$NewsID};
+
+        $NewsShown++;
+
         my %News = $PNObject->NewsGet(
             NewsID => $NewsID,
         );
@@ -57,7 +79,31 @@ sub Run {
             Name => 'News',
             Data => \%News,
         );
+
+        if( $ShowTeaserWidget ) {
+            $LayoutObject->Block(
+                Name => 'Teaser',
+                Data => {
+                    %News,
+                },
+            );
+        }
+
+        if( $ShowCreatedByWidget ) {
+            my %UserInformation = $UserObject->GetUserData(
+                UserID => $News{CreateBy},
+            );
+
+            $LayoutObject->Block(
+                Name => 'CreateByInformation',
+                Data => {
+                    %UserInformation,
+                },
+            );
+        }
     }
+
+    return 1 if !$NewsShown;
 
     my $Snippet = $LayoutObject->Output(
         TemplateFile => 'ProductNewsSnippet',
@@ -68,6 +114,27 @@ sub Run {
     }{
         $Snippet
     }xms;
+
+    my $JS = sprintf q~
+        $('span[id^="mark_as_read_"]').bind( 'click', function() {
+            var link    = $(this);
+            var news_id = link.attr('id').replace('mark_as_read_', '');
+
+            $.ajax({
+                type: "POST",
+                url:  '%s',
+                data : {
+                    Action: 'AgentProductNewsMarkRead',
+                    NewsID: news_id
+                },
+                success : function() {
+                    link.closest('tr').remove();
+                }
+            });
+        });
+    ~, $LayoutObject->{EnvRef}->{Baselink};
+
+    ${ $Param{Data} } =~ s{\z}{[% WRAPPER JSOnDocumentComplete %]\n$JS\n[% END %]}
 }
 
 1;
